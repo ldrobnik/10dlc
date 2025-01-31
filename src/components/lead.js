fc0c497babd520a4707f5f87c3a9cf400e334dc8
Łukasz Drobnik <drobniklukasz@gmail.com>
2025-01-31 08:09:04 +0100

    Adjust lead section style

--- src/components/lead.js
+++ src/components/lead.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Lead = ({ children }) => {
-  return <p style={{ fontSize: "1.1rem", fontStyle: "italic" }}>{children}</p>;
+  return <p style={{ fontSize: "1.1rem" }}>{children}</p>;
 };
 
 export default Lead;
